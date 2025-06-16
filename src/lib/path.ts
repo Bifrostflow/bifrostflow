1b7cf1d0ff9c55112d715c875c5241428b576f77
Chandravardhan <chandravardhansingh1@gmail.com>
2025-06-16 15:56:27 +0530

    added remote url

--- src/lib/path.ts
+++ src/lib/path.ts
@@ -1,2 +1,6 @@
-const dev = true;
-export const url = dev ? 'http://127.0.0.1:8000' : '';
+// const dev = false;
+// export const url = dev
+//   ? 'http://127.0.0.1:8000'
+//   : 'https://bifrost-atdm.onrender.com';
+
+export const url = 'https://bifrost-atdm.onrender.com';
