0806bb4f7ff9a843d82fb2f98ed5215139b0b222
Chandravardhan <chandravardhansingh1@gmail.com>
2025-06-28 12:25:56 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,3 @@
-import { testAuth } from '@/_backend/private/test';
-export default async function Dashboard({}: { children: React.ReactNode }) {
-  try {
-    const data = await testAuth();
-    console.log(data);
-    return <div>Dashboard {!!data && 'woho'}</div>;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (_e) {
-    return <h1>Error</h1>;
-  }
+export default function Dashboard({}: { children: React.ReactNode }) {
+  return <div>Dashboard</div>;
 }
