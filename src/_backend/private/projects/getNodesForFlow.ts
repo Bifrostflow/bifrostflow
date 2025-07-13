4f168c790c86589ca98374b74a655a3a1de79f7e
Chandravardhan <chandravardhansingh1@gmail.com>
2025-07-13 06:34:20 +0530

    documents section added

--- src/_backend/private/projects/getNodesForFlow.ts
+++ src/_backend/private/projects/getNodesForFlow.ts
@@ -37,3 +37,85 @@ export const getNodesForFlow = async (
     return { data: null, error: '', isSuccess: false, message: '' };
   }
 };
+
+export interface IFlowDocument {
+  name: string;
+  id: string;
+  updated_at: string;
+  created_at: string;
+  last_accessed_at: string;
+  metadata: {
+    eTag: string;
+    size: number;
+    mimetype: string;
+    cacheControl: string;
+    lastModified: string;
+    contentLength: number;
+    httpStatusCode: number;
+  };
+}
+
+export const getDocsForFlow = async (
+  flow_id: string,
+): Promise<APIResponse<IFlowDocument[]>> => {
+  const endpoint = `${url}/get-flow-docs?flow_id=${flow_id}`;
+  try {
+    const { getToken } = await auth();
+    const token = await getToken();
+
+    const res = await fetch(endpoint, {
+      method: 'GET',
+      next: {
+        revalidate: 60,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!res.ok) {
+      return { data: null, error: '', isSuccess: false, message: '' };
+    }
+
+    const json = await res.json();
+    console.log('RESPONSE: ', json);
+
+    return json;
+  } catch (error) {
+    console.error('Error Updated Nodes', error);
+    return { data: null, error: '', isSuccess: false, message: '' };
+  }
+};
+
+export const openDocForFlow = async (
+  flow_id: string,
+  name: string,
+): Promise<APIResponse<IFlowDocument[]>> => {
+  const endpoint = `${url}/open-doc?flow_id=${flow_id}&name=${name}`;
+  try {
+    const { getToken } = await auth();
+    const token = await getToken();
+
+    const res = await fetch(endpoint, {
+      method: 'GET',
+      next: {
+        revalidate: 60,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!res.ok) {
+      return { data: null, error: '', isSuccess: false, message: '' };
+    }
+
+    const json = await res.json();
+    console.log('RESPONSE: ', json);
+
+    return json;
+  } catch (error) {
+    console.error('Error Updated Nodes', error);
+    return { data: null, error: '', isSuccess: false, message: '' };
+  }
+};
